88aa44483a22b3b88c5ee0cc3146a1e5c5b580cf
Pedro R T A Horita <pedro_horita@hotmail.com>
2022-01-20 21:26:47 -0300

    Configuração inicial da aplicação
    Criação dos projetos projetos da aplicação.

---
+++ src/web/ES.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ES.WebApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
