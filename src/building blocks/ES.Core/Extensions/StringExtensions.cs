b1cadf873a172b1da58520237dbbb0d48d34f3fb
Pedro R T A Horita <pedro_horita@hotmail.com>
2022-01-24 20:12:55 -0300

    Criação models Cliente e Endereco
    Criação dos modelos de Cliente e de Endereço e os objetos de valor Email e Cpf

---
+++ src/building blocks/ES.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ApenasNumeros(this string str)
+        {
+            return new string(str.Where(char.IsDigit).ToArray());
+        }
+    }
+}
---
+++ src/services/ES.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using ES.Core.DomainObjects;
+using ES.Core.DomainObjects.Interfaces;
+using System;
+
+namespace ES.Clientes.API.Models
+{
+    public class Cliente : Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }   
+        public Email Email { get; private set; }   
+        public Cpf Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+
+        protected Cliente()
+        {
+        }
+
+        public Cliente(Guid id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = new(email);  
+            Cpf = new(cpf);  
+            Excluido = false;
+        }
+
+        public void TrocarEmail(string email)
+        {
+            Email = new(email);
+        }
+
+        public void AtribuirEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
+        }
+    }
+}
