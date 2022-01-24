701178a5e061686c079d42438372c8db30006e1b
Pedro R T A Horita <pedro_horita@hotmail.com>
2022-01-23 23:09:52 -0300

    Retry Pattern WebApp.MVC
    Implementação do Retry Pattern com Polly na aplicação web para comunicação com as APIs da do comercio eletrônico

--- src/web/ES.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/ES.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -4,7 +4,9 @@ using ES.WebApp.MVC.Services.Handlers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-
+using Polly;
+using System;
+using ES.WebApp.MVC.Extensions;
 
 namespace ES.WebApp.MVC.Configuration
 {
@@ -17,7 +19,13 @@ namespace ES.WebApp.MVC.Configuration
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
             services.AddHttpClient<ICatalogoService, CatalogoService>()
-                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                //.AddTransientHttpErrorPolicy(p => 
+                //    p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600)));
+                .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AddTransientHttpErrorPolicy(
+                    p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
+
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
